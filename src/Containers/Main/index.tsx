68ad71e0756f9fe7e0345472416c0753bca8e4e6
Iren1Paramonova <zerber17@ya.ru>
2023-01-19 21:38:50 +0300

    first commit

---
+++ src/Containers/Main/index.tsx
@@ -0,0 +1,11 @@
+import { PageWrapper } from 'Components/Common/PageWrapper';
+import { Content } from 'Components/Main/Content';
+import React from 'react';
+
+export const Main = () => {
+  return (
+    <PageWrapper>
+      <Content />
+    </PageWrapper>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+/* const h1 = React.createElement('h1', { className: 'title' }, 'Hello REACT');
+const div = React.createElement(
+  'div',
+  { style: { backgroundColor: 'black', width: '400px', height: '500PX' } },
+  h1,
+); */
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
